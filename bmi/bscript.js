7286cd5e554f1381e4631943a109241f8605863c
Utimama <agbasulai@yahoo.com>
2023-09-28 16:23:37 +0100

    first commit

--- bmi/bscript.js
+++ bmi/bscript.js
@@ -1,79 +1,39 @@
-body{
-    font-family: Arial, sans-serif;
-    text-align: center;
-}
-
-.container {
-    margin: 40px;
-}
-
-table {
-    margin: 0 auto;
-}
-
-th, td {
-    padding: 10px;
-}
-
-th {
-    background-color: #ccc;
-}
-
-.subject-score {
-    width: 60px;
-}
-
-button {
-    margin-top: 10px;
-    padding: 10px 20px;
-    background-color: #007BFF;
-    color: white;
-    border: none;
-    cursor: pointer;
-}
-
-button:hover {
-    background-color: #0056b3;
-}
-
-#result {
-    font-weight: bold;
-    margin-top: 10px;
-}
-
 function calculateAverage() {
-    var subject1 = parseFloat(document.getElementById("subject1").value);
-    var subject2 = parseFloat(document.getElementById("subject2").value);
-    var subject3 = parseFloat(document.getElementById("subject3").value);
-    var subject4 = parseFloat(document.getElementById("subject4").value);
-    var subject5 = parseFloat(document.getElementById("subject5").value);
+    let subject1 = parseFloat(document.getElementById("mathematics").value);
+    let subject2 = parseFloat(document.getElementById("english").value);
+    let subject3 = parseFloat(document.getElementById("physics").value);
+    let subject4 = parseFloat(document.getElementById("chemistry").value);
+    let subject5 = parseFloat(document.getElementById("biology").value);
+    let para1 = document.querySelector('#result')
+    let para2 = document.querySelector('#recommendation')
 
-    var total = subject1 + subject2 + subject3 + subject4 + subject5;
-    var average = total / 5;
+    let total = subject1 + subject2 + subject3 + subject4 + subject5;
+    let average = total / 5;
 
     if (!isNaN(average)) {
-        document.getElementById("result").innerHTML = "Average Score: " + average.toFixed(2);
+        para1.innerHTML = "Average Score: " + average.toFixed(2);
     } else {
-        document.getElementById("result").innerHTML = "Please enter valid scores for all subjects.";
+        para1.innerHTML = "Please enter valid scores for all subjects.";
     }
-}
 
-        var recommendation = "";
-        if (average >= 80) {
-            recommendation = "Excellent";
-        } else if (average >= 60) {
-            recommendation = "Average";
-        } else if (average >= 50) {
-            recommendation = "Below Average";
-        } else if (average >= 40) {
-            recommendation = "Pass";
-        } else {
-            recommendation = "Fail";
-        }
-        document.getElementById("recommendation").innerHTML = "Recommendation: " + recommendation;
+    average >= 70 ? para2.style.backgroundColor = 'green':
+    average >= 50 ? para2.style.backgroundColor = 'orange': 
+    para2.style.backgroundColor = 'red' 
+     
+    let recommendation = "";
+    if (average >= 80) {
+        recommendation = "Excellent";
+    } else if (average >= 60) {
+        recommendation = "Average";
+    } else if (average >= 50) {
+        recommendation = "Below Average";
+    } else if (average >= 40) {
+        recommendation = "Pass";
     } else {
-        document.getElementById("result").innerHTML = "Please enter valid scores for all subjects.";
+        recommendation = "Fail";
     }
+
+    para2.textContent = recommendation
 }
-<p id="recommendation"></p>
+        
 
